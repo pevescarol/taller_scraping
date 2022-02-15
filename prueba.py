22d9296e98a7c0ef4bfe1ed8cf8a35a47c92e907
pevescarol <carol.peves@gmail.com>
2022-02-15 14:18:27 -0300

    con bs4 pruebo extraer el title de la pag

--- prueba.py
+++ prueba.py
@@ -1,7 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 
 pagScrapy = "https://www.investing.com"
 
-page = requests.get(pagScrapy)
+head = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"}
 
-print(page)
+page = requests.get(pagScrapy, headers=head)
+
+soup = BeautifulSoup(page.text, "html.parser")
+
+print(soup.title.string)
